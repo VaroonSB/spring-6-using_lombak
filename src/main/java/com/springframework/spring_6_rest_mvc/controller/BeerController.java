1d9bd80c68d0810f133eed967a0093a19f11fbc7
VaroonSB <varoonsb1311@gmail.com>
2024-07-02 14:20:47 +0530

    added Location header in POST method

--- src/main/java/com/springframework/spring_6_rest_mvc/controller/BeerController.java
+++ src/main/java/com/springframework/spring_6_rest_mvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.springframework.spring_6_rest_mvc.model.Beer;
 import com.springframework.spring_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +23,9 @@ public class BeerController {
     public ResponseEntity handlePost(@RequestBody Beer beer) {
         Beer savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
