6c4a340ec68825992046f38632db24055b547451
VaroonSB <varoonsb1311@gmail.com>
2024-07-05 19:12:39 +0530

    added assertions in testcase using jsonPath and is()

--- src/test/java/com/springframework/spring_6_rest_mvc/controller/BeerControllerTest.java
+++ src/test/java/com/springframework/spring_6_rest_mvc/controller/BeerControllerTest.java
@@ -11,14 +11,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Slf4j
 @WebMvcTest(BeerController.class)
@@ -37,10 +37,13 @@ class BeerControllerTest {
         Beer testBeer = beerServiceImpl.getBeerList()
                                        .get(0);
 
-        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getId()).accept(MediaType.APPLICATION_JSON))
                .andExpect(status().isOk())
-               .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("$.id", is(testBeer.getId()
+                                                      .toString())))
+               .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
 }
