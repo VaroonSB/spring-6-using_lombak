a7e03bedee59fcd2fe2e1246aefc038028d32247
VaroonSB <varoonsb1311@gmail.com>
2024-11-11 17:36:54 +0530

    added test for deleteBeer

--- src/test/java/com/springframework/spring_6_rest_mvc/controller/BeerControllerTest.java
+++ src/test/java/com/springframework/spring_6_rest_mvc/controller/BeerControllerTest.java
@@ -7,6 +7,7 @@ import com.springframework.spring_6_rest_mvc.services.BeerServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 
@@ -96,11 +98,24 @@ class BeerControllerTest {
                                    .get(0);
 
         mockMvc.perform(put("/api/v1/beer/" + beer.getId()).accept(MediaType.APPLICATION_JSON)
-                                                          .contentType(MediaType.APPLICATION_JSON)
-                                                          .content(objectMapper.writeValueAsString(
-                                                                  beer)))
+                                                           .contentType(MediaType.APPLICATION_JSON)
+                                                           .content(objectMapper.writeValueAsString(
+                                                                   beer)))
                .andExpect(status().isNoContent());
 
         verify(beerService).updateBeerById(any(UUID.class), any(Beer.class));
+    }
+
+    @Test
+    void deleteBeer() throws Exception {
+        Beer beer = beerServiceImpl.getBeerList().get(0);
+
+        mockMvc.perform(delete("/api/v1/beer/" + beer.getId()).accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isNoContent());
+
+        ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
+        verify(beerService).deleteById(uuidArgumentCaptor.capture());
+
+        assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
     }
 }
