e47b9e01ea972c5c35d6c7ab0a31c2cf0016f5f3
VaroonSB <varoonsb1311@gmail.com>
2024-12-01 19:30:28 +0530

    added it for jpa services

---
+++ src/test/java/com/springframework/spring_6_rest_mvc/controller/BeerControllerIT.java
@@ -0,0 +1,46 @@
+package com.springframework.spring_6_rest_mvc.controller;
+
+import com.springframework.spring_6_rest_mvc.model.BeerDTO;
+import com.springframework.spring_6_rest_mvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void getBeerList() {
+        List<BeerDTO> beerDTOS = beerController.getBeerList();
+
+        assertThat(beerDTOS.size()).isGreaterThan(0);
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    void getEmptyBeerList() {
+        beerRepository.deleteAll();
+
+        List<BeerDTO> beerDTOS = beerController.getBeerList();
+
+        assertThat(beerDTOS.size()).isEqualTo(0);
+    }
+
+    @Test
+    void getBeerById() {
+    }
+}
